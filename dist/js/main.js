718e3af5169665e9bf2b8f8565b3647c9456d112
Holly Stone <hstone102@gmail.com>
2018-10-03 17:54:29 -0400

    more little changes, fixed removing event listener after a turn

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,32 +15,35 @@ function start() {
   // Create event listeners on every button
   let buttons = document.getElementsByClassName("space");
   for (let i = 0; i < buttons.length; i++) {
-    console.log("anything");
-    buttons[i].addEventListener("click", buttonLogic);
+    buttons[i].addEventListener("click", Move);
   }
 }
 
-function buttonLogic(event) {
+// function buttonLogic (event) {
+//
+//   let button = event.target;
+//   Move(button);
+//
+// }
+function Move(event) {
 
   let button = event.target;
-  console.log(button.id);
-  Move(button);
-}
-function Move(button) {
 
   if (counter % 2 == 0) {
-    console.log(counter);
     xTurn(button);
     counter++;
+    message.innerHTML = "It's O's Turn";
   } else {
     oTurn(button);
     counter++;
+    message.innerHTML = "It's X's Turn";
   }
 }
 function xTurn(button) {
   x.push(button.id);
   //read x input
   button.innerHTML = buttonTextX;
+  button.removeEventListener('click', Move);
   checkXWin(button);
   //go to y turn
 }
@@ -48,27 +51,26 @@ function xTurn(button) {
 function oTurn(button) {
   o.push(button.id);
   button.innerHTML = buttonTextO;
+  button.removeEventListener('click', Move);
   checkOWin(button);
 }
 
 function checkXWin(button) {
-  console.log(x);
   if (x.includes("1") && x.includes("2") && x.includes("3") || x.includes("4") && x.includes("5") && x.includes("6") || x.includes("7") && x.includes("8") && x.includes("9") || x.includes("1") && x.includes("4") && x.includes("7") || x.includes("2") && x.includes("5") && x.includes("8") || x.includes("3") && x.includes("6") && x.includes("9") || x.includes("1") && x.includes("5") && x.includes("9") || x.includes("3") && x.includes("5") && x.includes("7")) {
-    alert("X Wins.  Game Over");
+    message.innerHTML = "X Wins.";
+    removeClicksFromSpaces();
   } else {}
 }
 
 function checkOWin(button) {
-  console.log(o);
   if (o.includes("1") && o.includes("2") && o.includes("3") || o.includes("4") && o.includes("5") && o.includes("6") || o.includes("7") && o.includes("8") && o.includes("9") || o.includes("1") && o.includes("4") && o.includes("7") || o.includes("2") && o.includes("5") && o.includes("8") || o.includes("3") && o.includes("6") && o.includes("9") || o.includes("1") && o.includes("5") && o.includes("9") || o.includes("3") && o.includes("5") && o.includes("7")) {
-    alert("O Wins.  Game Over");
+    message.innerHTML = "O Won!";
+    removeClicksFromSpaces();
   } else {}
 }
 
-// function clear() {
-//
-// x = [];
-// y = [];
-// button.innerHTML = null;
-//
-// }
+function removeClicksFromSpaces() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].removeEventListener('click', Move);
+  }
+}
