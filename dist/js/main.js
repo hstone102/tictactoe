e6b60ec82bcf99dd6beeecd92f01c55d73863c62
Holly Stone <hstone102@gmail.com>
2018-10-04 19:44:44 -0400

    changes, fixing bugs

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,74 +3,84 @@ let o = [];
 let buttonTextX = "X";
 let buttonTextO = "O";
 let counter = 2;
+let buttons = document.getElementsByClassName("space");
 
 document.onreadystatechange = function () {
   if (document.readyState == "interactive") {
-    alert("Welcome to Tic Tac Toe.  X will go first.  Click where you want to go.");
+    document.getElementById('restartButton').onclick = start;
     start();
   }
 };
 
 function start() {
   // Create event listeners on every button
-  let buttons = document.getElementsByClassName("space");
+  counter = 2;
   for (let i = 0; i < buttons.length; i++) {
+    buttons[i].innerHTML = '';
+    //message.innerHTML("It's X's turn");
     buttons[i].addEventListener("click", Move);
   }
 }
 
-// function buttonLogic (event) {
-//
-//   let button = event.target;
-//   Move(button);
-//
-// }
 function Move(event) {
 
-  let button = event.target;
-
-  if (counter % 2 == 0) {
-    xTurn(button);
-    counter++;
-    message.innerHTML = "It's O's Turn";
+  let box = event.target;
+  counter++;
+  if (counter % 2 == 1) {
+    xTurn(box);
   } else {
-    oTurn(button);
-    counter++;
+    oTurn(box);
+  }
+}
+function xTurn(box) {
+  x.push(box.id);
+  //read x input
+  box.innerHTML = buttonTextX;
+  box.removeEventListener('click', Move);
+  checkXWin(box);
+  if (checkOWin == true) {
+    message.innerHTML("O Won!");
+    removeClicksFromSpaces();
+  } else {}
+}
+
+function oTurn(box) {
+  o.push(box.id);
+  box.innerHTML = buttonTextO;
+  box.removeEventListener('click', Move);
+  checkOWin(box);
+  if (checkOWin == true) {
+    message.innerHTML("O Won!");
+    removeClicksFromSpaces();
+  } else {
     message.innerHTML = "It's X's Turn";
   }
 }
-function xTurn(button) {
-  x.push(button.id);
-  //read x input
-  button.innerHTML = buttonTextX;
-  button.removeEventListener('click', Move);
-  checkXWin(button);
-  //go to y turn
-}
 
-function oTurn(button) {
-  o.push(button.id);
-  button.innerHTML = buttonTextO;
-  button.removeEventListener('click', Move);
-  checkOWin(button);
-}
-
-function checkXWin(button) {
+function checkXWin(box) {
   if (x.includes("1") && x.includes("2") && x.includes("3") || x.includes("4") && x.includes("5") && x.includes("6") || x.includes("7") && x.includes("8") && x.includes("9") || x.includes("1") && x.includes("4") && x.includes("7") || x.includes("2") && x.includes("5") && x.includes("8") || x.includes("3") && x.includes("6") && x.includes("9") || x.includes("1") && x.includes("5") && x.includes("9") || x.includes("3") && x.includes("5") && x.includes("7")) {
-    message.innerHTML = "X Wins.";
+    console.log("You're in x win");
+    message.innerHTML = "X Won.";
     removeClicksFromSpaces();
-  } else {}
+  } else {
+    message.innerHTML = "It's O's Turn";
+  }
 }
 
-function checkOWin(button) {
+function checkOWin(box) {
   if (o.includes("1") && o.includes("2") && o.includes("3") || o.includes("4") && o.includes("5") && o.includes("6") || o.includes("7") && o.includes("8") && o.includes("9") || o.includes("1") && o.includes("4") && o.includes("7") || o.includes("2") && o.includes("5") && o.includes("8") || o.includes("3") && o.includes("6") && o.includes("9") || o.includes("1") && o.includes("5") && o.includes("9") || o.includes("3") && o.includes("5") && o.includes("7")) {
-    message.innerHTML = "O Won!";
+    console.log("You're in y win");
+    message.innerHTML = "O Won.";
     removeClicksFromSpaces();
-  } else {}
+  } else {
+    message.innerHTML = "It's X's Turn";
+  }
 }
 
 function removeClicksFromSpaces() {
+  //let buttons = document.getElementsByClassName("space");
   for (let i = 0; i < buttons.length; i++) {
-    buttons[i].removeEventListener('click', Move);
+    console.log("you are in remove clicks");
+    buttons[i].removeEventListener('click', start);
   }
 }
